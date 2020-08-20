c4a284485da64911017f866a497d39a7109f55d6
posttsoi <deserza@gmail.com>
2020-08-20 21:42:23 +0300

    переделал модели (добавил корзину)

--- tattoo/tattoo_shop/models.py
+++ tattoo/tattoo_shop/models.py
@@ -1,8 +1,21 @@
 from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
-from django.utils import timezone
+from django.contrib.auth import get_user_model
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+
+
+User = get_user_model()
+
 
 # Create your models here.
+class Category(models.Model):
+    """категория товара"""
+    name = models.CharField("название категории", max_length=155)
+    slag = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
 
 
 class TattooSketch(models.Model):
@@ -10,23 +23,29 @@ class TattooSketch(models.Model):
     title = models.CharField('Название эскиза', max_length=125, blank=False)
     description = models.TextField('Описание эскиза', max_length=255, blank=False)
     vacant = models.BooleanField('свободен ли эскиз', default=True)
-
-
-class Cart(models.Model):
-    """Модель корзины """
-    pass
-
-
-class CartProduct(models.Model):
-    pass
+    image = models.ImageField(verbose_name='Изображение')
 
 
 class Product(models.Model):
-    pass
+
+    class Meta:
+        abstract = True
+
+    title = models.CharField('Название товара', max_length=125, blank=False)
+    description = models.TextField('Описание эскиза', max_length=255, blank=False)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveSmallIntegerField(default=0)
+    category = models.ForeignKey(Category, verbose_name="категория", on_delete=models.CASCADE)
+    slag = models.SlugField(unique=True)
+    image = models.ImageField(verbose_name='Изображение')
+
+    def __str__(self):
+        return self.title
 
 
 class Customer(models.Model):
     """Модель покупателя"""
+    user = models.ForeignKey(User, verbose_name='Пользователь', on_delete=models.CASCADE)
     email = models.EmailField('email address', blank=False)
     phone_number = PhoneNumberField('Номер телефона', unique=True, blank=False)
     name = models.CharField('Имя пользователя', max_length=30, blank=False)
@@ -34,13 +53,31 @@ class Customer(models.Model):
     patronymic = models.CharField('Отчество пользователя', max_length=30)
 
 
-class ProductPhoto(models.Model):
-    url = models.CharField(max_length=255)
-    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
+class CartProduct(models.Model):
+    """"""
+    customer = models.ForeignKey('Customer', on_delete=models.CASCADE, verbose_name="покупатель")
+    cart = models.ForeignKey('Cart', on_delete=models.CASCADE, verbose_name="корзина", related_name="related_products")
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+    quantity = models.PositiveSmallIntegerField(default=1, verbose_name='количество вещей в корзине')
+    final_price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='цена всех товаров в корзине')
+
+    def __str__(self):
+        return f"{self.product}"
 
 
-class TattooSketchPhoto(models.Model):
-    url = models.CharField(max_length=255)
-    sketch_id = models.ForeignKey(TattooSketch, on_delete=models.CASCADE)
+class Cart(models.Model):
+    """Модель корзины """
+
+    owner = models.ForeignKey('Customer', verbose_name='Владелец', on_delete=models.CASCADE)
+    products = models.ManyToManyField(CartProduct, blank=True, related_name='related_cart')
+    number_of_product = models.PositiveSmallIntegerField(default=0)
+    final_price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='цена всех товаров в корзине')
+
+    def __str__(self):
+        return self.id
+
+
 
 
