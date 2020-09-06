d823acadcc306c35c959641979e62f519415aa1b
Alexander Khoroshikh <xad71rus@gmail.com>
2020-09-06 21:26:48 +0300

    feat(router): allnext.js router events now forwarding to effector events

--- frontend/src/domain/router.ts
+++ frontend/src/domain/router.ts
@@ -2,23 +2,29 @@ import singletonRouter from 'next/router';
 
 import { root } from './root';
 
-export const routeChangeStarted = root.createEvent('routeChangeStarted');
+export const routerDomain = root.createDomain('router');
 
-routeChangeStarted.watch((...args) => console.log('r change:', args));
+export const routeChangeStart = routerDomain.createEvent('routeChangeStarted');
+export const routeChangeComplete = routerDomain.createEvent();
+export const routeChangeError = routerDomain.createEvent();
+export const beforeHistoryChange = routerDomain.createEvent();
+export const hashChangeStart = routerDomain.createEvent();
+export const hashChangeComplete = routerDomain.createEvent();
 
 const connectRouterToEffector = (singletonRouter) => {
-  if (singletonRouter.router) {
+  singletonRouter.ready(() => {
     const router = singletonRouter.router;
 
-    console.log('router loaded', router);
-
-    router.events.on('routeChangeStart', routeChangeStarted);
-  } else if (typeof window !== 'undefined') {
-    console.log('router not loaded, retry');
-    setTimeout(() => {
-      connectRouterToEffector(singletonRouter);
-    });
-  }
+    // forward next.js router events to effector events
+    router.events.on('routeChangeStart', routeChangeStart);
+    router.events.on('routeChangeComplete', routeChangeComplete);
+    router.events.on('routeChangeError', (err, url) =>
+      routeChangeError([err, url]),
+    );
+    router.events.on('beforeHistoryChange', beforeHistoryChange);
+    router.events.on('hashChangeStart', hashChangeStart);
+    router.events.on('hashChangeComplete', hashChangeComplete);
+  });
 };
 
 connectRouterToEffector(singletonRouter);
