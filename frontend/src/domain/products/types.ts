ec4eb83a586437ad51c472619fd77aea7654a50d
Alexander Khoroshikh <xad71rus@gmail.com>
2020-09-10 19:28:16 +0300

    feat(types): added types for products

---
+++ frontend/src/domain/products/types.ts
@@ -0,0 +1,39 @@
+export type TattooSketch = {
+  title: string;
+  description?: string;
+  vacant?: boolean;
+  image?: string;
+  slug: string;
+};
+
+export type Product = {
+  title: string;
+  description?: string;
+  price?: number;
+  quantity: number;
+  category: string;
+  image?: string;
+  slug: string;
+};
+
+export const Sizes = {
+  S: 'SMALL',
+  M: 'MEDIUM',
+  L: 'LARGE',
+  XL: 'EXTRA LARGE',
+} as const;
+
+export const Colours = {
+  B: 'BLACK',
+  W: 'WHITE',
+} as const;
+
+type ValueOf<T> = T[keyof T];
+
+export type Size = ValueOf<typeof Sizes>;
+export type Colour = ValueOf<typeof Colours>;
+
+export type TShirt = Product & {
+  size: Size;
+  color: Colour;
+};
