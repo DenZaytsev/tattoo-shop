b57620012333228766394cc442bb6a9c1171e49b
Alexander Khoroshikh <xad71rus@gmail.com>
2020-09-11 18:09:36 +0300

    refactor(lib/use-masonry): refactored use-masonry hook

--- frontend/lib/use-masonry/index.ts
+++ frontend/lib/use-masonry/index.ts
@@ -9,7 +9,7 @@ const setChildrenPositions = (children, { gap, columns }) => {
   });
 };
 
-export const layoutMasonry = (grid, isResizeAdaptable) => {
+export const layoutMasonry = (grid) => {
   grid.style.alignItems = 'start';
 
   const gap = parseFloat(getComputedStyle(grid).gridRowGap);
@@ -18,38 +18,22 @@ export const layoutMasonry = (grid, isResizeAdaptable) => {
       child.nodeType === 1 && +getComputedStyle(child).gridColumnEnd !== -1,
   );
 
-  let columns = 0;
+  const columns = getComputedStyle(grid).gridTemplateColumns.split(' ').length;
+  const rows = getComputedStyle(grid).gridTemplateRows.split(' ').length;
 
-  // get the post relayout number of columns and rows
-  const postLayoutColumns = getComputedStyle(grid).gridTemplateColumns.split(
-    ' ',
-  ).length;
-  const postLayoutRows = getComputedStyle(grid).gridTemplateRows.split(' ')
-    .length;
+  if (!(rows > 1)) return () => {}; // makes no sense to masonry-layout one or less rows
 
-  if (!(postLayoutRows > 1)) return; // makes no sense to masonry-layout one or less rows
-
-  // if the number of columns has changed
-  if (columns !== postLayoutColumns) {
-    // update number of columns
-    columns = postLayoutColumns;
-
-    if (isResizeAdaptable) {
-      // revert to initial positioning, no margin
-      children.forEach((c) => c.style.removeProperty('margin-top'));
-    }
-
-    // if we have more than one column
-    if (columns > 1) {
-      setChildrenPositions(children, { gap, columns });
-    }
+  // if we have more than one column
+  if (columns > 1) {
+    children.forEach((c) => c.style.removeProperty('margin-top'));
+    setChildrenPositions(children, { gap, columns });
   }
 };
 
 /**
  * @param gridRef - React ref to dom-node with display: grid;
  */
-export const useMasonry = (gridRef, isListeningToResize = true) => {
+export const useMasonry = (gridRef) => {
   const resizeCallbackRef = useRef();
 
   useEffect(() => {
@@ -57,8 +41,7 @@ export const useMasonry = (gridRef, isListeningToResize = true) => {
       gridRef.current !== null &&
       getComputedStyle(gridRef.current).gridTemplateRows !== 'masonry'
     ) {
-      resizeCallbackRef.current = () =>
-        layoutMasonry(gridRef.current, isListeningToResize);
+      resizeCallbackRef.current = () => layoutMasonry(gridRef.current);
 
       resizeCallbackRef.current();
 
