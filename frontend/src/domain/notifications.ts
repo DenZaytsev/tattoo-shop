973b0190339e9a45c43aaf0f9a3ba17cd7b85e1c
Alexander Khoroshikh <xad71rus@gmail.com>
2020-09-12 18:17:40 +0300

    fix(types): fixed types import in notifications.ts

--- frontend/src/domain/notifications.ts
+++ frontend/src/domain/notifications.ts
@@ -1,8 +1,8 @@
-import { Toast } from '@geist-ui/react';
+import type { Toast } from '@geist-ui/react/dist/use-toasts/use-toast';
 
 import { root } from './root';
 
-export const notify = root.createEvent('notification');
+export const notify = root.createEvent<Toast>('notification');
 export const $currentNotification = root.createStore<Toast | null>(null);
 
 $currentNotification.on(notify, (_, toast: Toast) => toast);
