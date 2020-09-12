c5c806480aa1672993fe20df528a680051ff50a6
Alexander Khoroshikh <xad71rus@gmail.com>
2020-09-12 16:51:20 +0300

    feat(product-card): added basic flexible behavior for prod-card

--- frontend/src/features/product-card/index.tsx
+++ frontend/src/features/product-card/index.tsx
@@ -1,9 +1,53 @@
 import React from 'react';
 import Link from 'next/link';
 import { Card, Text } from '@geist-ui/react';
+import { css } from 'linaria';
+
+import { AspectRatioKeeper } from '../../../lib/aspect-ratio-keeper';
 import type { AnyProduct } from '../../domain/products/types';
 
 type ProductCardProps = AnyProduct;
+
+const productCard = css`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, auto));
+  grid-auto-rows: auto;
+  grid-gap: 16px;
+
+  width: 100%;
+  position: relative;
+`;
+
+const productImage = css`
+  & img {
+    object-fit: cover;
+    object-position: center;
+    width: 100%;
+    height: 100%;
+    border-radius: 5px;
+  }
+`;
+
+const productContent = css`
+  display: flex;
+  flex-flow: column nowrap;
+`;
+
+const productDescription = css`
+  max-width: 250px;
+`;
+
+const productLink = css`
+  &::before {
+    content: ' ';
+    display: block;
+    position: absolute;
+    top: 0;
+    right: 0;
+    left: 0;
+    bottom: 0;
+  }
+`;
 
 export const ProductCard: React.FC<ProductCardProps> = ({
   title,
@@ -12,15 +56,21 @@ export const ProductCard: React.FC<ProductCardProps> = ({
   vacant,
 }) => {
   return (
-    <Card>
-      <img src={image} alt="" />
-      <Text h3>
-        <Link href="/" passHref>
-          <a>{title}</a>
-        </Link>
-      </Text>
-      <Text>{description}</Text>
-      <Text>{vacant ? 'вакантно' : 'занят'}</Text>
+    <Card shadow>
+      <Card.Content className={productCard}>
+        <AspectRatioKeeper className={productImage} aspectRatio={3 / 4}>
+          <img src={image} alt="" />
+        </AspectRatioKeeper>
+        <div className={productContent}>
+          <Text h3>
+            <Link href="/" passHref>
+              <a className={productLink}>{title}</a>
+            </Link>
+          </Text>
+          <Text className={productDescription}>{description}</Text>
+          <Text>{vacant ? 'вакантно' : 'занят'}</Text>
+        </div>
+      </Card.Content>
     </Card>
   );
 };
