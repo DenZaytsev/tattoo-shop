a2c5d7b396aa5cf125d88b14bb4dd190997eef8d
Alexander Khoroshikh <xad71rus@gmail.com>
2020-09-14 18:42:19 +0300

    feat(lib/lazy-image): rebuild lozad-provider into a hook

--- frontend/lib/lazy-image/index.tsx
+++ frontend/lib/lazy-image/index.tsx
@@ -1,34 +1,75 @@
 import React from 'react';
 import lozad from 'lozad';
+import { useRouter } from 'next/router';
+import { styled } from 'linaria/react';
 
-export const LozadProvider: React.FC = () => {
-  React.useLayoutEffect(() => {
+import { useIsomorphicLayoutEffect } from '../use-isomorphic-layout-effect';
+
+const getLozad = () =>
+  lozad('.lozad', {
+    rootMargin: '10px 10px',
+    threshold: 0.1,
+    enableAutoReload: true,
+  });
+
+export const useLozad = () => {
+  const { pathname } = useRouter();
+  const lozadRef = React.useRef(null);
+
+  useIsomorphicLayoutEffect(() => {
     if (window) {
-      const observer = lozad();
+      if (!lozadRef.current) {
+        lozadRef.current = getLozad();
+      }
 
-      observer.observe();
+      lozadRef.current.observe();
     }
-  }, []);
-
-  return null;
+  }, [lozadRef.current, pathname]);
 };
 
 interface LazyImageProps {
   src: string;
   alt?: string;
   placeholder?: string;
+  className?: string;
+  style?: React.CSSProperties;
 }
+
+interface PlaceholderProps {
+  placeholder: string;
+  className?: string;
+}
+
+export const Placeholder: React.FC<PlaceholderProps> = styled.div`
+  background: ${({ placeholder }) => placeholder};
+  width: 100%;
+  height: 100%;
+`;
 
 export const LazyImage: React.FC<LazyImageProps> = ({
   src = '',
   alt = '',
   placeholder = 'white',
-}) => (
-  <img
-    className="lozad"
-    src="empty" // added to hide ugly border in img with empty src attribute
-    data-src={src}
-    data-placeholder-background={placeholder}
-    alt={alt}
-  />
-);
+  className,
+  style,
+}) => {
+  if (!src) {
+    return (
+      <Placeholder
+        placeholder={placeholder}
+        className={className}
+        style={style}
+      />
+    );
+  }
+
+  return (
+    <img
+      className={`lozad ${className}`}
+      style={style}
+      data-src={src}
+      data-placeholder-background={placeholder}
+      alt={alt}
+    />
+  );
+};
