8fcd762902e9709427681f6fcd9db203f2924c08
Alexander Khoroshikh <xad71rus@gmail.com>
2020-09-18 17:23:20 +0300

    fix(hot-fix): effector-logger is buggy with next.js ssr
    so for now its turned off

--- frontend/src/core/root.ts
+++ frontend/src/core/root.ts
@@ -1,8 +1,3 @@
 import { createDomain } from 'effector';
-import { attachLogger } from 'effector-logger/attach';
 
 export const root = createDomain('root');
-
-if (process.env.NODE_ENV === 'development') {
-  attachLogger(root);
-}
