106935dd1928e7cc07b9e0c5e7932832daff5e84
Alexander Khoroshikh <xad71rus@gmail.com>
2020-09-18 16:58:32 +0300

    feat(dev-mode): added effector-logger for dev env

--- frontend/src/core/root.ts
+++ frontend/src/core/root.ts
@@ -1,3 +1,8 @@
 import { createDomain } from 'effector';
+import { attachLogger } from 'effector-logger/attach';
 
 export const root = createDomain('root');
+
+if (process.env.NODE_ENV === 'development') {
+  attachLogger(root);
+}
