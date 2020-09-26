7cf5041f99482a6111f518bf43cf62a264de2d41
Alexander Khoroshikh <xad71rus@gmail.com>
2020-09-27 00:02:55 +0300

    fix(format-money): no 00-tail for int prices

--- frontend/lib/format-ru-money/index.ts
+++ frontend/lib/format-ru-money/index.ts
@@ -3,9 +3,13 @@ export const RUBLE = '₽';
 const DELIM = ',';
 
 export const formatRuMoney = (price: number | string): string => {
-  const priceStr = `${Number(price).toFixed(2)}`
+  let priceStr = `${Number(price).toFixed(2)}`
     .replace(/\d(?=(\d{3})+\.)/g, '$& ')
     .replace('.', DELIM);
+
+  if (priceStr.slice(-2) === '00') {
+    priceStr = priceStr.slice(0, -3);
+  }
 
   return `${priceStr} ${RUBLE}`;
 };
