83f930e3b1433fd453d71f9234d9d2a7efaa845d
Alexander Khoroshikh <xad71rus@gmail.com>
2020-10-06 22:43:07 +0300

    chore(dev-deps): added effector-logger to browser bundle

--- frontend/src/core/root.ts
+++ frontend/src/core/root.ts
@@ -1,3 +1,11 @@
 import { createDomain } from 'effector';
 
+import { isBrowser } from '../../lib/is-browser';
+
 export const root = createDomain('root');
+
+if (process.env.NODE_ENV === 'development' && isBrowser()) {
+  import('effector-logger/attach').then(({ attachLogger }) => {
+    attachLogger(root);
+  });
+}
